55cb577aa97041263ad7e69b4102fdbc64ad4f81
samah ahmed <samah.ahmed.aly@gmail.com>
2022-10-14 11:12:37 +0300

    project structure

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'app.dart';
+import 'bloc_observer.dart';
+
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
